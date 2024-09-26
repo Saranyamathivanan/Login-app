a431ff3642ce65ad4371882b8237b31398e9ccf4
Saranyamathivanan <saranya.mathi2@gmail.com>
2024-09-26 16:08:03 +0900

    madlibs.py added

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+adj =input("Adjective:")
+verb1 = input("Verb:")
+noun = input("Noun:")
+
+madlib = f"Welcome to this page {noun}! I am {verb1} on this computer and its very {adj} fast"
+
+
+print (madlib)
